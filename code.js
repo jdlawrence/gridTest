23be009f81f4092c541234d70ccf57961900c2a8
jdlawrence <jamil.lawrence@gmail.com>
2017-06-28 16:18:23 -0700

    Removes old column class when resizing

--- code.js
+++ code.js
@@ -1,25 +1,33 @@
-var columns = document.getElementsByClassName('column');
 
-function applyClassToColumns (newClass) {
+function resize() {
+  function applyClassToColumns(newClass) {
+    for (var i = 0; i < columns.length; i++) {
+      // Must remove old classes first
+      columns[i].classList.remove('col-1-1', 'col-1-2', 'col-1-4');
+      columns[i].classList.add(newClass);
+    }
+  }
 
-  for (var i = 0; i < columns.length; i++) {
-    columns[i].classList.add(newClass);
+  var columns = document.getElementsByClassName('column');
+
+  var width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+  var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+  if (width < 360) {
+    applyClassToColumns('col-1-1');
+  } else if (width < 720) {
+    applyClassToColumns('col-1-2');
+  } else {
+    applyClassToColumns('col-1-4');
   }
 }
 
-var width = window.innerWidth
-  || document.documentElement.clientWidth
-  || document.body.clientWidth;
+// Call resize when the window first loads
+resize();
 
-var height = window.innerHeight
-  || document.documentElement.clientHeight
-  || document.body.clientHeight;
-
-if (width < 360) {
-  applyClassToColumns('col-1-1');
-} else if (width < 720) {
-  applyClassToColumns('col-1-2');
-} else {
-  applyClassToColumns('col-1-4');
-}
-alert(width + ':' + height);
+window.onresize = resize;
