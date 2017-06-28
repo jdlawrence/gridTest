de288a3446e5d3292fb2f212fc6b2500289c7095
jdlawrence <jamil.lawrence@gmail.com>
2017-06-28 16:01:00 -0700

    Adds dynamic classes dynamically

--- code.js
+++ code.js
@@ -1,6 +1,25 @@
 var columns = document.getElementsByClassName('column');
-console.log(columns);
 
-for (var i = 0; i < columns.length; i++) {
-  columns[i].classList.add('col-1-4');
+function applyClassToColumns (newClass) {
+
+  for (var i = 0; i < columns.length; i++) {
+    columns[i].classList.add(newClass);
+  }
 }
+
+var width = window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+
+var height = window.innerHeight
+  || document.documentElement.clientHeight
+  || document.body.clientHeight;
+
+if (width < 360) {
+  applyClassToColumns('col-1-1');
+} else if (width < 720) {
+  applyClassToColumns('col-1-2');
+} else {
+  applyClassToColumns('col-1-4');
+}
+alert(width + ':' + height);
